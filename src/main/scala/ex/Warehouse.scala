cf4af2c1289aa9f1cc72fec781325f6b6d9a082d
Valerio-Giannini <valerio.giannini@studio.unibo.it>
2024-04-08 15:09:08 +0200

    ex2 before refactoring

--- src/main/scala/ex/Warehouse.scala
+++ src/main/scala/ex/Warehouse.scala
@@ -2,13 +2,20 @@ package ex
 
 import util.Optionals.Optional
 import util.Sequences.*
+import util.Sequences.Sequence.*
 trait Item:
   def code: Int
   def name: String
   def tags: Sequence[String]
 
 object Item:
-  def apply(code: Int, name: String, tags: Sequence[String] = Sequence.empty): Item = ???
+  def apply(code: Int, name: String, tags: Sequence[String] = Sequence.empty): Item =
+    ItemImpl(code, name, tags)
+
+  private case class ItemImpl(override val code: Int,
+                              override val name: String,
+                              override val tags: Sequence[String] = Sequence.empty) extends Item
+
 
 /**
  * A warehouse is a place where items are stored.
@@ -45,18 +52,39 @@ trait Warehouse:
 end Warehouse
 
 object Warehouse:
-  def apply(): Warehouse = ???
+  def apply(items: Sequence[Item]): Warehouse =
+    WarehouseImpl(items)
+
+  private case class WarehouseImpl(var items: Sequence[Item]) extends Warehouse:
+
+    def store(item: Item): Unit =
+      items = items.concat(Cons(item, Nil()))
+
+    def searchItems(tag: String): Sequence[Item] =
+      items.filter(i => i.tags.contains(tag))
+
+    def retrieve(code: Int): Optional[Item] =
+      items.find(i => i.code == code)
+
+    def remove(item: Item): Unit =
+      items.filter(i => i != item)
+
+    def contains(itemCode: Int): Boolean =
+      val listCode: Sequence[Int] = items.map(i => i.code)
+      !listCode.find(_ == itemCode).isEmpty
+
+
 
 @main def mainWarehouse(): Unit =
-  val warehouse = Warehouse()
+  val warehouse = Warehouse(Nil())
 
   val dellXps = Item(33, "Dell XPS 15", Sequence("notebook"))
   val dellInspiron = Item(34, "Dell Inspiron 13", Sequence("notebook"))
   val xiaomiMoped = Item(35, "Xiaomi S1", Sequence("moped", "mobility"))
 
-  warehouse.contains(dellXps.code) // false
+  println(warehouse.contains(dellXps.code))// false
   warehouse.store(dellXps) // side effect, add dell xps to the warehouse
-  warehouse.contains(dellXps.code) // true
+  println(warehouse.contains(dellXps.code)) // true
   warehouse.store(dellInspiron) // side effect, add dell Inspiron to the warehouse
   warehouse.store(xiaomiMoped) // side effect, add xiaomi moped to the warehouse
   warehouse.searchItems("mobility") // Sequence(xiaomiMoped)
